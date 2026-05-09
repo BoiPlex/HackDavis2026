2e3274a0a38008d6a864e558404748492c934da1
BoiPlex <endplex@gmail.com>
2026-05-09 12:56:55 -0700

    Connect frontend + backend + database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from db import db
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Connected!"}
+
+@app.get("/users")
+async def get_users():
+    users = await db.users.find().to_list(length=100)
+    return {"users": users}
